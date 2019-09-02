0a313b4623518f167a45e0d53544d668337b36b0
Blade Barringer <blade@crookedneighbor.com>
2019-09-01 21:37:51 -0500

    Add mutation observer

---
+++ src/js/lib/dom-node-ready.js
@@ -0,0 +1,46 @@
+// adapted from http://ryanmorr.com/using-mutation-observers-to-watch-for-element-availability/
+
+let observer
+const listeners = []
+
+export function reset () {
+  observer = null
+  listeners.splice(0, listeners.length)
+}
+
+export function ready (selector, fn) {
+  // Store the selector and callback to be monitored
+  listeners.push({
+    selector: selector,
+    fn: fn
+  })
+  if (!observer) {
+    // Watch for changes in the document
+    observer = new global.MutationObserver(check)
+    observer.observe(document.documentElement, {
+      childList: true,
+      subtree: true
+    })
+  }
+
+  // Check if the element is currently in the DOM
+  check()
+}
+
+function check () {
+  // Check the DOM for elements matching a stored selector
+  listeners.forEach((listener) => {
+    // Query for elements matching the specified selector
+    const elements = Array.from(document.querySelectorAll(listener.selector))
+
+    elements.forEach((element) => {
+      // Make sure the callback isn't invoked with the
+      // same element more than once
+      if (!element.ready) {
+        element.ready = true
+        // Invoke the callback with the element
+        listener.fn.call(element, element)
+      }
+    })
+  })
+}
---
+++ test/lib/dom-node-ready.test.js
@@ -0,0 +1,109 @@
+import {
+  reset,
+  ready
+} from '../../src/js/lib/dom-node-ready'
+
+describe('domNodeReady', function () {
+  beforeEach(function () {
+    sandbox.spy(global, 'MutationObserver')
+  })
+
+  afterEach(function () {
+    reset()
+  })
+
+  it('applies transformation to specified dom nodes right away', function () {
+    const div = this.makeDiv()
+    const div2 = this.makeDiv()
+    const spy = sandbox.stub()
+
+    document.querySelectorAll.withArgs('.class-name').returns([
+      div,
+      div2
+    ])
+
+    ready('.class-name', spy)
+
+    expect(spy.callCount).to.equal(2)
+    expect(spy).to.be.calledWith(div)
+    expect(spy).to.be.calledWith(div2)
+  })
+
+  it('adds check to mutation observer', function () {
+    const spy = sandbox.stub()
+
+    ready('.class-name', spy)
+
+    expect(global.MutationObserver.calledWithNew()).to.equal(true)
+    expect(global.MutationObserver).to.be.calledWith(sandbox.match.func)
+  })
+
+  it('only creates observer once', function () {
+    const spy = sandbox.stub()
+
+    ready('.class-name', spy)
+
+    expect(global.MutationObserver.callCount).to.equal(1)
+    expect(global.MutationObserver.calledWithNew()).to.equal(true)
+    expect(global.MutationObserver).to.be.calledWith(sandbox.match.func)
+
+    ready('.another-class-name', spy)
+
+    expect(global.MutationObserver.callCount).to.equal(1)
+  })
+
+  it('applies transformation to specified dom nodes only once', function () {
+    const div = this.makeDiv()
+    const div2 = this.makeDiv()
+    const spy = sandbox.stub()
+
+    document.querySelectorAll.withArgs('.class-name').returns([
+      div,
+      div2
+    ])
+
+    ready('.class-name', spy)
+
+    expect(spy.callCount).to.equal(2)
+    expect(spy).to.be.calledWith(div)
+    expect(spy).to.be.calledWith(div2)
+
+    ready('.class-name', spy)
+
+    expect(spy.callCount).to.equal(2)
+  })
+
+  it('applies transformation only to new dom nodes', function () {
+    const div = this.makeDiv()
+    const div2 = this.makeDiv()
+    const spy = sandbox.stub()
+
+    document.querySelectorAll.withArgs('.class-name').returns([
+      div,
+      div2
+    ])
+
+    ready('.class-name', spy)
+
+    const handler = global.MutationObserver.args[0][0]
+
+    expect(spy.callCount).to.equal(2)
+    expect(spy).to.be.calledWith(div)
+    expect(spy).to.be.calledWith(div2)
+
+    spy.resetHistory()
+
+    const div3 = this.makeDiv()
+
+    document.querySelectorAll.withArgs('.class-name').returns([
+      div,
+      div3,
+      div2
+    ])
+
+    handler()
+
+    expect(spy.callCount).to.equal(1)
+    expect(spy).to.be.calledWith(div3)
+  })
+})
