f385f5b8a689d953b5e843d3bf5aec338bd6c863
Blade Barringer <blade@crookedneighbor.com>
2019-09-01 21:39:11 -0500

    Set up test files

--- test/_helper.test.js
+++ test/_helper.test.js
@@ -2,12 +2,52 @@ import chrome from 'sinon-chrome/extensions'
 import sinon from 'sinon'
 import sinonChai from 'sinon-chai'
 import chai from 'chai'
+import bus from 'framebus'
 
 chai.use(sinonChai)
 
 global.chrome = chrome
 global.sandbox = sinon.createSandbox()
 global.expect = chai.expect
+
+beforeEach(function () {
+  this.makeDiv = function (attributes = {}) {
+    const defaultConfig = {
+      innerHTML: '',
+      parentNode: {
+        appendChild: sandbox.stub(),
+        removeChild: sandbox.stub()
+      },
+      addEventListener: sandbox.stub(),
+      getAttribute: sandbox.stub().returns(''),
+      hasAttribute: sandbox.stub(),
+      setAttribute: sandbox.stub(),
+      classList: {
+        add: sandbox.stub(),
+        remove: sandbox.stub()
+      },
+      style: {},
+      querySelector: sandbox.stub()
+    }
+
+    return Object.assign({}, attributes, defaultConfig)
+  }
+
+  const createElement = sandbox.stub()
+
+  createElement.withArgs('div').returns(this.makeDiv())
+
+  global.MutationObserver = function () {
+  }
+  global.MutationObserver.prototype.observe = sandbox.stub()
+  global.document = {
+    querySelectorAll: sandbox.stub().returns([]),
+    createElement: createElement,
+    getElementById: sandbox.stub()
+  }
+  sandbox.stub(bus, 'on')
+  sandbox.stub(bus, 'emit')
+})
 
 afterEach(function () {
   sandbox.restore()
