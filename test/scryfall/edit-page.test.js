54d1f35e2462cf321528b3e31d78b34280c15079
Blade Barringer <blade@crookedneighbor.com>
2019-09-03 22:54:32 -0500

    Add shell for tests

---
+++ test/scryfall/edit-page.test.js
@@ -0,0 +1,7 @@
+describe('scryfall edit page', function () {
+  it('makes an edhrec button', function () {
+    // TODO
+  })
+
+  // TODO more tests
+})
---
+++ test/scryfall/open-ehdrec-frame.test.js
@@ -0,0 +1,25 @@
+describe('open edhrec frame', function () {
+  it('uses scryfall to populate the iframe src', function () {
+    // TODO
+  })
+
+  it('listens for the EDHREC_READY event and replies with cards in the deck', function () {
+    // TODO
+  })
+
+  it('unhides the modal content when EDHREC_READY event fires', function () {
+    // TODO
+  })
+
+  it('listens for the ADD_CARD_FROM_EDHREC event for nonland card and emits an ADD_CARD_TO_DECK event', function () {
+    // TODO
+  })
+
+  it('listens for the ADD_CARD_FROM_EDHREC event for land card and emits an ADD_CARD_TO_DECK event', function () {
+    // TODO
+  })
+
+  it('adds the ifraem to the page', function () {
+    // TODO
+  })
+})
