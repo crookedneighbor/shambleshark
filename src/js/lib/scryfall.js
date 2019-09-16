2603411203d734d9f5b47c424c8d175a78d89fe1
Blade Barringer <blade@crookedneighbor.com>
2019-09-16 09:25:37 -0500

    Cache deck values for getDeck

--- src/js/lib/scryfall.js
+++ src/js/lib/scryfall.js
@@ -1,12 +1,26 @@
 import bus from 'framebus'
 import ScryfallClient from 'scryfall-client'
 
+const CACHE_TIMEOUT_FOR_DECK_REQUESTS = 2000 // 2 seconds
+
+let getDeckPromise
+
 export const api = new ScryfallClient()
 
 export function getDeck () {
-  return new Promise((resolve) => {
+  if (getDeckPromise) {
+    return getDeckPromise
+  }
+
+  getDeckPromise = new Promise((resolve) => {
     bus.emit('REQUEST_DECK', resolve)
   })
+
+  setTimeout(() => {
+    getDeckPromise = null
+  }, CACHE_TIMEOUT_FOR_DECK_REQUESTS)
+
+  return getDeckPromise
 }
 
 export default {
--- test/lib/scryfall.test.js
+++ test/lib/scryfall.test.js
@@ -9,6 +9,14 @@ describe('scryfall', function () {
   })
 
   describe('getDeck', function () {
+    beforeEach(function () {
+      jest.useFakeTimers()
+    })
+
+    afterEach(function () {
+      jest.runAllTimers()
+    })
+
     it('requests deck from Scryfall page', async function () {
       let deck = {}
 
@@ -16,10 +24,39 @@ describe('scryfall', function () {
         cb(deck)
       })
 
-      let resolvedDeck = await getDeck()
+      const resolvedDeck = await getDeck()
 
       expect(resolvedDeck).toEqual(deck)
       expect(bus.emit).toBeCalledWith('REQUEST_DECK', expect.any(Function))
+    })
+
+    it('caches result if request is made a second time within 2 seconds', async function () {
+      const firstDeck = { id: '1' }
+      const secondDeck = { id: '2' }
+
+      bus.emit.mockImplementationOnce((event, cb) => {
+        cb(firstDeck)
+      })
+      bus.emit.mockImplementationOnce((event, cb) => {
+        cb(secondDeck)
+      })
+
+      const resolvedDeck = await getDeck()
+
+      jest.advanceTimersByTime(1999)
+
+      const compareDeck = await getDeck()
+
+      expect(resolvedDeck).toEqual(firstDeck)
+      expect(resolvedDeck).toEqual(compareDeck)
+      expect(compareDeck).toEqual(firstDeck)
+
+      jest.advanceTimersByTime(2)
+
+      const deckAfterTimeout = await getDeck()
+
+      expect(deckAfterTimeout).not.toEqual(resolvedDeck)
+      expect(deckAfterTimeout).toEqual(secondDeck)
     })
   })
 })
