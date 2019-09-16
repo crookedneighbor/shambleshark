38df233673181c394d93b3eaa2f51fd6df94c061
Blade Barringer <blade@crookedneighbor.com>
2019-09-15 22:01:32 -0500

    Add lib for detecting if deck is a commander deck

---
+++ src/js/lib/deck-parser.js
@@ -0,0 +1,35 @@
+import scryfall from './scryfall-client'
+
+export function isCommanderLikeDeck (deck) {
+  return Promise.resolve(Boolean(deck.entries.commanders))
+}
+
+export function hasLegalCommanders (commanders) {
+  if (commanders.length === 0) {
+    // no commanders in commander section
+    return Promise.resolve(false)
+  }
+
+  if (!commanders.find(c => c.card_digest)) {
+    // has entries in commander section of deck, but none of them
+    // have a card digest yet, so we can't calculate commanders
+    return Promise.resolve(false)
+  }
+
+  return Promise.all(commanders.map((card) => {
+    if (!card.card_digest) {
+      // ignore blank lines
+      return true
+    }
+
+    return scryfall.get('/cards/search', {
+      q: `!"${card.card_digest.name}" is:commander`
+    })
+  })).then(() => {
+    // if all promises resolve, all were commanders
+    return true
+  }).catch(() => {
+    // if even one promise 404s, then not all were commanders
+    return false
+  })
+}
---
+++ test/lib/deck-parser.test.js
@@ -0,0 +1,103 @@
+import {
+  hasLegalCommanders,
+  isCommanderLikeDeck
+} from '../../src/js/lib/deck-parser'
+import scryfall from '../../src/js/lib/scryfall-client'
+
+describe('Deck Parser', function () {
+  describe('isCommanderLikeDeck', function () {
+    it('returns false if deck has no commanders section', async function () {
+      const deck = {
+        entries: {
+          lands: [],
+          nonlands: []
+        }
+      }
+
+      expect(isCommanderLikeDeck(deck)).resolves.toBe(false)
+    })
+
+    it('returns true if deck has a commanders section', async function () {
+      const deck = {
+        entries: {
+          commanders: [],
+          lands: [],
+          nonlands: []
+        }
+      }
+
+      expect(isCommanderLikeDeck(deck)).resolves.toBe(true)
+    })
+  })
+
+  describe('hasLegalCommanders', function () {
+    beforeEach(function () {
+      jest.spyOn(scryfall, 'get')
+    })
+
+    it('returns false if deck has a commanders section, but no commanders', async function () {
+      const commanders = []
+
+      await expect(hasLegalCommanders(commanders)).resolves.toBe(false)
+    })
+
+    it('returns true if deck has a commanders section, but no card digest info in section', async function () {
+      const commanders = [{
+        id: 'id'
+      }, {
+        id: 'other-id'
+      }]
+
+      await expect(hasLegalCommanders(commanders)).resolves.toBe(false)
+    })
+
+    it('returns true if deck has a commanders section and all cards in it are legal commanders', async function () {
+      const commanders = [{
+        card_digest: {
+          name: 'Sidar Kondo of Jamuraa'
+        },
+        id: 'id'
+      }, {
+        card_digest: {
+          name: 'Tana the Bloodsower'
+        },
+        id: 'other-id'
+      }]
+
+      scryfall.get.mockResolvedValue({})
+
+      await expect(hasLegalCommanders(commanders)).resolves.toBe(true)
+      expect(scryfall.get).toBeCalledWith('/cards/search', {
+        q: '!"Sidar Kondo of Jamuraa" is:commander'
+      })
+      expect(scryfall.get).toBeCalledWith('/cards/search', {
+        q: '!"Tana the Bloodsower" is:commander'
+      })
+    })
+
+    it('returns false if deck has a commanders section and any cards in it are not legal commanders', async function () {
+      const commanders = [{
+        card_digest: {
+          name: 'Tana the Bloodsower'
+        },
+        id: 'id'
+      }, {
+        card_digest: {
+          name: 'Craterhoof Behemoth'
+        },
+        id: 'other-id'
+      }]
+
+      scryfall.get.mockResolvedValueOnce({})
+      scryfall.get.mockRejectedValueOnce(new Error('404'))
+
+      await expect(hasLegalCommanders(commanders)).resolves.toBe(false)
+      expect(scryfall.get).toBeCalledWith('/cards/search', {
+        q: '!"Tana the Bloodsower" is:commander'
+      })
+      expect(scryfall.get).toBeCalledWith('/cards/search', {
+        q: '!"Craterhoof Behemoth" is:commander'
+      })
+    })
+  })
+})
