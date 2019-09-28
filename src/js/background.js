8d086f9f5b072c4bc1e57e77fb56014c0baff493
Blade Barringer <blade@crookedneighbor.com>
2019-09-27 21:32:43 -0500

    Add TODO for background task when extension updates

--- src/js/background.js
+++ src/js/background.js
@@ -1,2 +1,13 @@
 import '../img/icon-128.png'
 import '../img/icon-34.png'
+
+chrome.runtime.onInstalled.addListener(function (details) {
+  if (details.reason === 'install') {
+    // TODO welcome screen?
+  } else if (details.reason === 'update') {
+    const version = chrome.runtime.getManifest().version
+
+    // TODO apply defaults for any new features
+    console.log('Updated from ' + details.previousVersion + ' to ' + version + '!')
+  }
+})
