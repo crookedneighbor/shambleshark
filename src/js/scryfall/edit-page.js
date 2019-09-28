1d81ed44fa498b2c34e1ca97ffbe1380d941980a
Blade Barringer <blade@crookedneighbor.com>
2019-09-27 21:35:36 -0500

    Only run feature if it is enabled

--- src/js/scryfall/edit-page.js
+++ src/js/scryfall/edit-page.js
@@ -1,16 +1,15 @@
 import bus from 'framebus'
-import EDHRecSuggestions from '../features/edhrec-suggestions'
+import deckbuilderFeatures from '../features/deck-builder-features'
 
 export default function () {
   // to allow push notifications to appear above modals
   document.querySelector('.left-tray').style.zIndex = 9000
 
   bus.on('SCRYFALL_LISTENERS_READY', function () {
-    return Promise.all([
-      EDHRecSuggestions
-    ].map(function (Feature) {
+    Promise.all(deckbuilderFeatures.map(function (Feature) {
       const feature = new Feature()
-      return feature.run()
+
+      return feature.isEnabled().then(isEnabled => isEnabled && feature.run())
     }))
   })
 }
