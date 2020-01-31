bfd91b608ed592cba4306eb577495816fbde1ee2
Blade Barringer <blade@crookedneighbor.com>
2020-01-31 13:13:39 -0600

    Fix issue where page could not be parsed with a query string

--- src/js/scryfall/index.js
+++ src/js/scryfall/index.js
@@ -5,7 +5,7 @@ export default function start () {
   embed()
 
   const urlPieces = window.location.href.split('/')
-  const page = urlPieces[urlPieces.length - 1].split('#')[0]
+  const page = urlPieces[urlPieces.length - 1].split('#')[0].split('?')[0]
 
   // TODO separate scripts for each page or jsut do it this way?
   if (page === 'build') {
