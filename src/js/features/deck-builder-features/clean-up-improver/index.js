c678b6d95c90fb56557766dcf5db15a962c67131
Blade Barringer <blade@crookedneighbor.com>
2020-02-07 11:23:14 -0600

    Emit MODIFY_CLEAN_UP in cleanup improver

--- src/js/features/deck-builder-features/clean-up-improver/index.js
+++ src/js/features/deck-builder-features/clean-up-improver/index.js
@@ -1,21 +1,29 @@
 import Feature from '../../feature'
 import { sections } from '../../constants'
+import bus from 'framebus'
 
 class CleanUpImprover extends Feature {
   async run () {
-    // TODO
+    const settings = await CleanUpImprover.getSettings()
+    bus.emit('MODIFY_CLEAN_UP', settings)
   }
 }
 
 CleanUpImprover.metadata = {
   id: 'clean-up-improver',
-  futureFeature: true,
   title: 'Clean Up Improver',
   section: sections.DECK_BUILDER,
   description: 'Modifies the clean up button to better organize the deck.'
 }
 CleanUpImprover.settingsDefaults = {
-  enabled: true
+  enabled: true,
+  cleanUpLandsInSingleton: true
 }
+
+CleanUpImprover.settingDefinitions = [{
+  id: 'cleanUpLandsInSingleton',
+  label: 'Move lands and nonlands to their correct columns when cleaning up a singleton deck',
+  input: 'checkbox'
+}]
 
 export default CleanUpImprover
---
+++ test/features/cleanup-improver/index.test.js
@@ -0,0 +1,26 @@
+import CleanUpImprover from '../../../src/js/features/deck-builder-features/clean-up-improver'
+import bus from 'framebus'
+
+describe('Clean Up Improver', function () {
+  describe('run', function () {
+    beforeEach(function () {
+      jest.spyOn(bus, 'emit').mockImplementation()
+      jest.spyOn(CleanUpImprover, 'getSettings').mockResolvedValue({
+        enabled: true,
+        foo: 'bar'
+      })
+    })
+
+    it('emits a MODIFY_CLEAN_UP', async function () {
+      const cui = new CleanUpImprover()
+
+      await cui.run()
+
+      expect(bus.emit).toBeCalledTimes(1)
+      expect(bus.emit).toBeCalledWith('MODIFY_CLEAN_UP', {
+        enabled: true,
+        foo: 'bar'
+      })
+    })
+  })
+})
