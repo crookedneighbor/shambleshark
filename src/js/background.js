f731000fb935645ae57f70285733163e96232b12
Blade Barringer <blade@crookedneighbor.com>
2020-02-28 15:12:19 -0600

    Cleanup linting issues

--- src/js/background.js
+++ src/js/background.js
@@ -23,16 +23,16 @@ onInstalled().addListener(function (details) {
 onHeadersReceived({
   addListener (info) {
     const headers = info.responseHeaders.filter(rawHeader => {
-      const header = rawHeader.name.toLowerCase();
+      const header = rawHeader.name.toLowerCase()
 
       return header !== 'x-frame-options' && header !== 'frame-options'
     })
 
-    return {responseHeaders: headers};
+    return { responseHeaders: headers }
   },
   config: {
-    urls: [ '*://tagger.scryfall.com/*' ],
-    types: [ 'sub_frame' ]
+    urls: ['*://tagger.scryfall.com/*'],
+    types: ['sub_frame']
   },
   permissions: [
     'blocking',
