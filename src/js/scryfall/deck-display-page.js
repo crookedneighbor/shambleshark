bfed67171641b6b21f28a3d20b698fa32222b55d
Blade Barringer <blade@crookedneighbor.com>
2020-04-08 22:01:29 -0500

    Add sidebar container for deck display features

--- src/js/scryfall/deck-display-page.js
+++ src/js/scryfall/deck-display-page.js
@@ -1,8 +1,19 @@
 import bus from 'framebus'
 import deckViewFeatures from 'Features/deck-view-features'
+import mutation from 'Lib/mutation'
+import createElement from 'Lib/create-element'
 import {
   BUS_EVENTS as events
 } from 'Constants'
+
+function addContainerForSidebarFeatures () {
+  const section = createElement(`<div id="shambleshark-deck-display-sidebar-toolbox" class="sidebar-toolbox">
+  </div>`).firstChild
+
+  mutation.ready('.sidebar', container => {
+    container.appendChild(section)
+  })
+}
 
 export default function () {
   bus.on(events.SCRYFALL_LISTENERS_READY, function () {
@@ -10,6 +21,8 @@ export default function () {
       const feature = new Feature()
 
       return feature.isEnabled().then(isEnabled => isEnabled && feature.run())
-    }))
+    })).then(() => {
+      addContainerForSidebarFeatures()
+    })
   })
 }
