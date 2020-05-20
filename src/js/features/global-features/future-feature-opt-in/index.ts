c3a463ff06b595ba9b8947ba0f38336773e5d1b4
Blade Barringer <blade@crookedneighbor.com>
2020-05-20 13:10:46 -0500

    Convert global features to ts

---
+++ src/js/features/global-features/future-feature-opt-in/index.ts
@@ -0,0 +1,22 @@
+import Feature from "Feature";
+import { FEATURE_IDS as ids, FEATURE_SECTIONS as sections } from "Constants";
+
+class FutureFeatureOptIn extends Feature {
+  static metadata = {
+    id: ids.FutureFeatureOptIn,
+    title: "Opt-In To New Features Automatically",
+    section: sections.GLOBAL,
+    description:
+      "When a new feature is added to the extension, automatically turn it on! Disabling this just means you will need to enable each new feature individually.",
+  };
+
+  static settingsDefaults = {
+    enabled: true,
+  };
+
+  async run() {
+    // NOOP only controls install/update behavior
+  }
+}
+
+export default FutureFeatureOptIn;
