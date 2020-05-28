9059870e2100ff55fc931e7a3784b754eed56d39
Blade Barringer <blade@crookedneighbor.com>
2020-05-28 08:23:54 -0500

    Convert edhrec code to TS

--- src/js/edhrec/ready.ts
+++ src/js/edhrec/ready.ts
@@ -2,8 +2,21 @@ import bus from "framebus";
 import iframe from "Lib/iframe";
 import { BUS_EVENTS as events } from "Constants";
 
-function getRecs({ commanders, cards }) {
-  return global
+type GetRecsOptions = {
+  commanders: string[];
+  cards: string[];
+};
+
+type GetRecsPayload = {
+  errors?: Error[];
+  [prop: string]: unknown;
+};
+
+function getRecs({
+  commanders,
+  cards,
+}: GetRecsOptions): Promise<GetRecsPayload> {
+  return window
     .fetch("https://edhrec.com/api/recs/", {
       method: "POST",
       body: JSON.stringify({
@@ -24,7 +37,7 @@ export default function start() {
   }
 
   bus.on(events.REQUEST_EDHREC_RECOMENDATIONS, function (payload, reply) {
-    getRecs(payload)
+    getRecs(payload as GetRecsOptions)
       .then((result) => {
         if (result.errors) {
           reply([result]);
