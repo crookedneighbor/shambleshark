a8e0ff271725c0b41d8aab6712a8c81ecd3ecec9
Blade Barringer <blade@crookedneighbor.com>
2020-10-28 13:32:26 -0500

    rename variable

--- src/js/lib/create-element.ts
+++ src/js/lib/create-element.ts
@@ -1,13 +1,13 @@
 type Container = "body" | "head";
 
 export default function createElement<T extends Element = Element>(
-  string: string,
+  htmlString: string,
   options?: { container: Container }
 ): T {
   const container = options?.container || "body";
   const fragment = document.createDocumentFragment();
   const parser = new window.DOMParser();
-  const parsed: Document = parser.parseFromString(string, "text/html");
+  const parsed: Document = parser.parseFromString(htmlString, "text/html");
   const elements = parsed.getElementsByTagName(container)[0].children;
 
   Array.from(elements).forEach((e) => fragment.appendChild(e));
