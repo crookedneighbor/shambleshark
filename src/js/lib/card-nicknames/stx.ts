a73e3f938d152128b730fb5cdd8f432ae382261f
Blade Barringer <blade@crookedneighbor.com>
2021-03-30 22:35:14 -0500

    chore: add additional stx nicknames

--- src/js/lib/card-nicknames/stx.ts
+++ src/js/lib/card-nicknames/stx.ts
@@ -1,11 +1,101 @@
 // Strixhaven (STX)
 export default [
   {
+    // https://scryfall.com/card/stx/63/a-poet-that-knows-it
+    setCode: "stx",
+    collectorNumber: "63",
+    realName: [],
+    nickname: ["A Poet That Knows It"],
+    source: "Scryfall Preview Name",
+  },
+  {
+    // https://scryfall.com/card/stx/64/sinister-domain
+    setCode: "stx",
+    collectorNumber: "64",
+    realName: [],
+    nickname: ["Sinisiter Domain"],
+    source: "Scryfall Preview Name",
+  },
+  {
+    // https://scryfall.com/card/stx/68/deep-disappointment
+    setCode: "stx",
+    collectorNumber: "68",
+    realName: [],
+    nickname: ["Deep Disappointment"],
+    source: "Scryfall Preview Name",
+  },
+  {
+    // https://scryfall.com/card/stx/70/eyebat
+    setCode: "stx",
+    collectorNumber: "70",
+    realName: [],
+    nickname: ["Eyebat"],
+    source: "Scryfall Preview Name",
+  },
+  {
+    // https://scryfall.com/card/stx/91/academic-debate
+    setCode: "stx",
+    collectorNumber: "91",
+    realName: [],
+    nickname: ["Academic Debate"],
+    source: "Scryfall Preview Name",
+  },
+  {
+    // https://scryfall.com/card/stx/92/forum-mooderator
+    setCode: "stx",
+    collectorNumber: "92",
+    realName: [],
+    nickname: ["Forum Mooderator"],
+    source: "Scryfall Preview Name",
+  },
+  {
+    // https://scryfall.com/card/stx/93/geist-commander
+    setCode: "stx",
+    collectorNumber: "93",
+    realName: [],
+    nickname: ["Geist Commander"],
+    source: "Scryfall Preview Name",
+  },
+  {
+    // https://scryfall.com/card/stx/94/conspiracy-theorist
+    setCode: "stx",
+    collectorNumber: "94",
+    realName: [],
+    nickname: ["Conspiracy Theorist"],
+    source: "Scryfall Preview Name",
+  },
+  {
+    // https://scryfall.com/card/stx/96/i-have-a-dragon-and-dragon-says-no-do-you-have-a-dragon%3F-didnt-think-so
+    setCode: "stx",
+    collectorNumber: "96",
+    realName: [],
+    nickname: [
+      "I Have a Dragon and Dragon Says 'No'. Do You Have a Dragon? Didn't Think So",
+    ],
+    source: "Scryfall Preview Name",
+  },
+  {
+    // https://scryfall.com/card/stx/114/well-do-it-live!
+    setCode: "stx",
+    collectorNumber: "114",
+    realName: [],
+    nickname: ["We'll Do It Live!"],
+    source: "Scryfall Preview Name",
+  },
+  {
     // https://scryfall.com/card/stx/132/nightmares-beverage
     setCode: "stx",
     collectorNumber: "132",
     realName: [],
     nickname: ["Nightmare's Beverage"],
+    source: "Scryfall Preview Name",
+  },
+  {
+    // https://scryfall.com/card/stx/135/hungry-chomp-rider
+    setCode: "stx",
+    collectorNumber: "135",
+    realName: [],
+    nickname: ["Hungry Chomp Rider"],
     source: "Scryfall Preview Name",
   },
   {
@@ -39,6 +129,30 @@ export default [
     collectorNumber: "186",
     realName: [],
     nickname: ["Sublime Rehearsal"],
+    source: "Scryfall Preview Name",
+  },
+  {
+    // https://scryfall.com/card/stx/187/frog-fractals
+    setCode: "stx",
+    collectorNumber: "187",
+    realName: [],
+    nickname: ["Frog Fractals"],
+    source: "Scryfall Preview Name",
+  },
+  {
+    // https://scryfall.com/card/stx/202/malestrom-muse
+    setCode: "stx",
+    collectorNumber: "202",
+    realName: [],
+    nickname: ["Malestrom Muse"],
+    source: "Scryfall Preview Name",
+  },
+  {
+    // https://scryfall.com/card/stx/209/oggyar-battle-seer
+    setCode: "stx",
+    collectorNumber: "209",
+    realName: [],
+    nickname: ["Oggyar Battle Seer"],
     source: "Scryfall Preview Name",
   },
 ];
